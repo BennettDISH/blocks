8a7fddac77c9f0416e34867c5f0ae26e33ca6dda
BennettDISH <43225774+BennettDISH@users.noreply.github.com>
2024-05-22 15:03:41 -0600

    linking data

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 // /main.js
-import { TextBlock } from './my-web-components/components/text-block.js';
-import { VideoBlock } from './my-web-components/components/video-block.js';
-import { SwapBlock } from './my-web-components/components/swap-block.js';
+import { TextBlock } from './components/text-block.js';
+import { VideoBlock } from './components/video-block.js';
+import { SwapBlock } from './components/swap-block.js';
 
-customElements.define('text-block', TextBlock);
-customElements.define('video-block', VideoBlock);
-customElements.define('swap-block', SwapBlock);
+if (!customElements.get('text-block')) {
+  customElements.define('text-block', TextBlock);
+}
+if (!customElements.get('video-block')) {
+  customElements.define('video-block', VideoBlock);
+}
+if (!customElements.get('swap-block')) {
+  customElements.define('swap-block', SwapBlock);
+}
