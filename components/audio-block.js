4f8065c862d6fb295271b4737a567094938864b1
BennettDISH <43225774+BennettDISH@users.noreply.github.com>
2024-05-28 17:47:04 -0600

    Added Audio block

---
+++ components/audio-block.js
@@ -0,0 +1,50 @@
+// /components/audio-block.js
+
+export default class AudioBlock extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+    this.props = { src: '' };
+    this.shadowRoot.innerHTML = `
+      <style>
+        .audio-block {
+          width: 100%;
+          height: 100%;
+          box-sizing: border-box;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+        audio {
+          width: 100%;
+        }
+      </style>
+      <div class="audio-block">
+        <audio controls>
+          <source src="" type="audio/mpeg">
+          Your browser does not support the audio element.
+        </audio>
+      </div>
+    `;
+  }
+
+  set data(value) {
+    this.props = value;
+    this.render();
+  }
+
+  get data() {
+    return this.props;
+  }
+
+  connectedCallback() {
+    this.render();
+  }
+
+  render() {
+    const audioElement = this.shadowRoot.querySelector('audio source');
+    audioElement.src = this.props.src || '';
+  }
+}
+
+customElements.define('audio-block', AudioBlock);
